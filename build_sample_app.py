8dcd76ddd30aa8db388c536ea1e5587bceebc338
silabs-mehdi <mehdi.marzougui@silabs.com>
2026-01-12 14:20:51 +0100

    CI pipeline v1.0

--- build_sample_app.py
+++ build_sample_app.py
@@ -3,18 +3,10 @@ import sys
 import os
 import xml.etree.cElementTree as ET
 import subprocess
-import re
 from contextlib import chdir
 from shutil import copytree
 
-# import openai
 
-# TODO: Would probably be nice to use Pathlib for all paths instead of plain strings
-
-GENERATION_SUCCESS_PATTERN = re.compile(r".*Project generation completed.*")
-BUILD_ERROR_PATTERN = re.compile(r".*error:.*")
-BUILD_WARNING_PATTERN = re.compile(r".*warning:.*")
-litellm_client = None
 
 def parse_xml(xml_file: str, predefined_examples: list[str], predefined_boards: list[str]) -> dict[str,list[str]]:
     """ Collect the sample app paths and supported boards, and return as dict. """
@@ -52,19 +44,15 @@ def generate_project(slcp_path:str, app_board_specific_path:str, board:str):
     except subprocess.CalledProcessError:
         raise
 
-def handle_generation_error(e: subprocess.CalledProcessError, project_path: str, failed_generations: list) -> None:
+def handle_generation_error(e: subprocess.CalledProcessError, project_path: str) -> None:
     os.makedirs(project_path)
     with open(f"{project_path}/generation_error.log", "w") as f:
-        f.write(e.stdout)
-    project_name = project_path.split("/")[-1]
-    failed_generations.append(project_name)
-    # instruction = f"Do not incorporate the SLF4J logger warning. Incorporate the project name: {project_name}. Analyse the following project generation log and determine the reason of its failure in two to three sentences: "
-    # ask_ai(instruction, e.stderr, "litellm_failed_generations")
+        f.write(e.stderr)
 
 def handle_build_error(e: subprocess.CalledProcessError, project_name: str) -> None:
     os.makedirs(project_name)
     with open(f"{project_name}/build_error.log", "w") as f:
-        f.write(e.stderr)
+        f.write(e.stdout)
 
 def compile_project(cmake_path:str):
     """ Compile the project using make. """
@@ -79,55 +67,10 @@ def compile_project(cmake_path:str):
                         )            
         except subprocess.CalledProcessError:
             raise
-def check_final_results(failed_generations: list, failed_builds: list, warning_builds: list) -> bool:
-    " Print the failed projects. "
-    success = True        
-    if len(failed_generations) > 0:
-        success = False
-        print(f"Failed to generate {len(failed_generations)} projects:")
-        for failed_project in failed_generations:
-            print(failed_project)
-        print("\n")
-    if len(failed_builds) > 0:
-        success = False
-        print(f"Failed to build {len(failed_builds)} projects:")
-        for failed_build in failed_builds:
-            print(failed_build)
-        print("\n")
-    if len(warning_builds) > 0:
-        print(f"Warning during the build of {len(warning_builds)} projects:")
-        for warning_build in warning_builds:
-            print(warning_build)
-        print("\n")
-    
-    litellm_failed_generations = open("tmp/litellm_failed_generations")
-    litellm_failed_builds = open("tmp/litellm_failed_builds")
-    instruction = "Format it nicely for a Slack message, keep it short and plain, return only with the message. A unique project name consist the project identifier and the board, such as project_id_board. Summarize and group the following Jenkins pipeline errors by project_id, only mention the project_id once per error, only list maximum three of the boards per error: "
-    ask_ai(instruction, f"{litellm_failed_generations.read()} {litellm_failed_builds.read()}", "litellm_summary")
-
-    return success
-
-def ask_ai(instruction:str, log:str, file:str) -> str:
-    """ Send the failed project to AI for further explanations. """
-    response = litellm_client.chat.completions.create(model="gpt-4.1", messages = [
-        {
-            "role": "user",
-            "content": f"{instruction}{log}"
-        }
-    ])
-
-    litellm = open(f"tmp/{file}", "a")
-    litellm.write(response.choices[0].message.content)
-    litellm.close()
             
-def build_apps(sample_apps: dict, sample_apps_root: str, output_dir: str) -> list: 
+def build_apps(sample_apps: dict, sample_apps_root: str, output_dir: str) -> None: 
     """ Take the parsed XML-content, generate projects, and compile them. Failures are collected using regexes. """
-    failed_generations = []
-    # failed_builds = []
-    # warning_builds = []
-
     successful_generation_dir = 'tmp/successful_generations'
-
     successful_builds_dir = output_dir + '/successful_builds'
     failed_generations_dir = output_dir + '/failed_generations'
     failed_builds_dir = output_dir + '/failed_builds'
@@ -137,9 +80,6 @@ def build_apps(sample_apps: dict, sample_apps_root: str, output_dir: str) -> lis
         # app_name ex: soc_advertising_manufacturer_specific_data
         slcp_path = sample_apps_root + '/' + app_path
         app_name = app_path.split('/')[-1].split('.')[0]
-
-        # app_parent_dir_failure = failed_generations_dir + '/'+ app_name
-        # os.makedirs(app_parent_dir_failure)
         
         # Iterate over the projects supported boards
         boards = sample_apps[app_path]
@@ -152,7 +92,7 @@ def build_apps(sample_apps: dict, sample_apps_root: str, output_dir: str) -> lis
                 generate_project(slcp_path, app_board_specific_path, board)
             except subprocess.CalledProcessError as e:
                 app_board_specific_path = failed_generations_dir + '/' + app_name + '/' + project_name
-                handle_generation_error(e, app_board_specific_path,failed_generations)
+                handle_generation_error(e, app_board_specific_path)
                 print(f"failed to generate project {project_name}, details logged at {app_board_specific_path}")
                 continue
             #generation succeeded, proceeed to build
@@ -164,32 +104,8 @@ def build_apps(sample_apps: dict, sample_apps_root: str, output_dir: str) -> lis
                 app_board_specific_path = failed_builds_dir + '/' + app_name + '/' + project_name
                 handle_build_error(e, app_board_specific_path)
                 print(f"failed to build project {project_name}, details logged at {app_board_specific_path}")
-                           
-    
-    return 0, 0, 0 
-             
-    #         # Check if generation_success_pattern can be found in the log, else start building
-    #         if not GENERATION_SUCCESS_PATTERN.search(stdout):  
-    #             failed_generations.append(project_name)
-    #             print(f"Generating {project_name} failed!\n")
-    #             # instruction = f"Do not incorporate the SLF4J logger warning. Incorporate the project name: {project_name}. Analyse the following project generation log and determine the reason of its failure in two to three sentences: "
-    #             # ask_ai(instruction, stdout, "litellm_failed_generations")
-    #         # If generations succeeds, build the application
-    #         else:
-    #             stdout = compile_project(project_path)
-    #             print(stdout)
-                
-    #             if BUILD_ERROR_PATTERN.search(stdout):
-    #                 failed_builds.append(project_name)
-    #                 print(f"Building {project_name} failed!\n")
-    #                 # instruction = f"Do not incorporate the SLF4J logger warning. Incorporate the project name: {project_name}. Analyse the following project build log and determine the reason of its failure in two to three sentences: "
-    #                 # ask_ai(instruction, stdout, "litellm_failed_builds")
-    #                 continue
-                
-    #             if BUILD_WARNING_PATTERN.search(stdout):
-    #                 warning_builds.append(project_name)
-    #                 print(f"Warning in {project_name}!\n")
-    # return failed_generations, failed_builds, warning_builds
+
+
 
 if __name__ == "__main__":
     sample_apps_root = sys.argv[1]
@@ -197,14 +113,8 @@ if __name__ == "__main__":
     predefined_examples = [] if sys.argv[2].strip() == "" else sys.argv[2].split(',')
     predefined_boards = [] if sys.argv[3].strip() == "" else sys.argv[3].split(',')
     output_dir = sys.argv[4]
-    # litellm_client = openai.OpenAI(api_key=sys.argv[4], base_url="https://litellm.silabs.net/")
     xml_file = sample_apps_root + '/templates.xml'
     sample_apps = parse_xml(xml_file, predefined_examples, predefined_boards)
     print("Done.")
     print("Building apps...")
-    failed_generations, failed_builds, warning_builds = build_apps(sample_apps=sample_apps, sample_apps_root=sample_apps_root, output_dir=output_dir)
-    # success = check_final_results(failed_generations, failed_builds, warning_builds)
-    # if success == True: 
-    #     print("SUCCESS")
-    # else:
-    #     raise Exception("FAILURE")
+    build_apps(sample_apps=sample_apps, sample_apps_root=sample_apps_root, output_dir=output_dir)
